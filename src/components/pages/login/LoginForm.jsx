8266e0663309fa24a0cf768e1783fa48d5883298
Tom <t.landes84@gmail.com>
2023-12-13 15:12:48 +0100

    oups

---
+++ src/components/pages/login/LoginForm.jsx
@@ -0,0 +1,26 @@
+function LoginForm() {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Hello ${inputValue}`);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        placeholder="Enter your name"
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default LoginForm;
